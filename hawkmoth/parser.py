a24d279b0ba26016ecaa80d739169014236da1df
Peder Toftegaard Olsen <pdr@theperfectapp.com>
2019-11-08 12:38:28 +0100

    Diagnose might not refer to a file.

--- hawkmoth/parser.py
+++ hawkmoth/parser.py
@@ -266,7 +266,7 @@ def clang_diagnostics(errors, diagnostics):
            4: ErrorLevel.ERROR}
 
     for diag in diagnostics:
-        errors.extend([(sev[diag.severity], diag.location.file.name,
+        errors.extend([(sev[diag.severity], diag.location.file.name if diag.location.file else "<No File>",
                         diag.location.line, diag.spelling)])
 
 # return a list of (comment, metadata) tuples
